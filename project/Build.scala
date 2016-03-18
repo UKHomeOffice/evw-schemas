019c5c9650e2486cc951885b05dd8b88a2899cb6
jackmaloney <jack.maloney@transformuk.com>
2016-03-18 14:20:45 +0000

    Versioning updates: - Updated scala version to 2.11.8 - Updated sbt version to 0.13.11 - Update rtp-test-lib and rt-io-lib to latest versions

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ object Build extends Build {
       name := moduleName,
       organization := "uk.gov.homeoffice",
       version := "1.0-SNAPSHOT",
-      scalaVersion := "2.11.7",
+      scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
         "-language:implicitConversions",
@@ -47,10 +47,10 @@ object Build extends Build {
 
     module.settings(
       libraryDependencies ++= Seq(
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.4.0-SNAPSHOT" withSources()
+        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.7.0" withSources()
       ),
       libraryDependencies ++= Seq(
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.0-SNAPSHOT" % Test classifier "tests" withSources(),
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.4.0-SNAPSHOT" % Test classifier "tests" withSources()))
+        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.1-SNAPSHOT" % Test classifier "tests" withSources(),
+        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.7.0" % Test classifier "tests" withSources()))
   }
 }
