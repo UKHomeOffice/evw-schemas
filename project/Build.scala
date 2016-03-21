47b3558e4069c09c68227390ab55d22065893f78
Tamer Moustafa <tamer.moustafa@transformuk.com>
2016-03-21 14:21:19 +0000

    @nojira - updating versions

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.1.0",
+      version := "1.1.1",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
