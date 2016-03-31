4a6a879f2b29528e7f099f9c4101f5577a9ddd23
Jay Keshur <jaykeshur.homeoffice@gmail.com>
2016-03-31 15:45:09 +0100

    Update scala version to snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.1.4",
+      version := "1.1.5-SNAPSHOT",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
