f010bbe13e004c8c65a8d6142f702f88e14f1397
Ralph Cowling <speak@ralph-cowling.com>
2016-07-04 16:26:47 +0100

    snapshot lol

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.5.3",
+      version := "1.5.4-SNAPSHOT",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
