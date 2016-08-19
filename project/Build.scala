0a1ed68f1b28ef88d662cdc10dc988892e4a99bc
jackmaloney <jack.maloney@transformuk.com>
2016-08-19 13:23:42 +0100

    Update version number for dependencies

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.7.1",
+      version := "1.7.2-SNAPSHOT",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
@@ -28,10 +28,10 @@ object Build extends Build {
         "Sonatype Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/",
         "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"),
       libraryDependencies ++= Seq(
-      "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.2" withSources(),
-      "uk.gov.homeoffice" %% "rtp-io-lib" % "1.7.2" withSources()),
+      "uk.gov.homeoffice" %% "rtp-test-lib" % "1.3.1" withSources(),
+      "uk.gov.homeoffice" %% "rtp-io-lib" % "1.7.17" withSources()),
       libraryDependencies ++= Seq(
-        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.2.2" % Test classifier "tests" withSources(),
-        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.7.2" % Test classifier "tests" withSources())
+        "uk.gov.homeoffice" %% "rtp-test-lib" % "1.3.1" % Test classifier "tests" withSources(),
+        "uk.gov.homeoffice" %% "rtp-io-lib" % "1.7.17" % Test classifier "tests" withSources())
     )
 }
