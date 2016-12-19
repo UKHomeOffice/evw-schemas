41494b75558c106c9d88512f7ea28399cd5ea235
David Thompson <davethompson21@gmail.com>
2016-12-19 10:57:14 +0000

    Scala SNAPSHOT version added

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "1.8.1",
+      version := "1.8.2-SNAPSHOT",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
