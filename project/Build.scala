be4868ae23506e01df22e0fbba6f18d6eebd8c84
David Thompson <davethompson21@gmail.com>
2017-02-17 13:38:52 +0000

    Removing SNAPSHOT from version 4.0.2

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "4.0.2-SNAPSHOT",
+      version := "4.0.2",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
