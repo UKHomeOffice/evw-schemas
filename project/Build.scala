41591e52d2fead68c6da3de46ce3377a84345555
jackmaloney <jack.maloney@transformuk.com>
2017-06-15 14:51:35 +0100

    Increase to 4.1.3 snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object Build extends Build {
     .settings(
       name := moduleName,
       organization := "uk.gov.homeoffice",
-      version := "4.1.2",
+      version := "4.1.3-SNAPSHOT",
       scalaVersion := "2.11.8",
       scalacOptions ++= Seq(
         "-feature",
